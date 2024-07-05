e8790891a75d96d5c6c77c21b23e6c795a3819df
mirceap24 <24mirceap@gmail.com>
2024-07-05 10:53:19 +0300

    Implement hash function

--- src/hash_table.c
+++ src/hash_table.c
@@ -35,3 +35,16 @@ void ht_del_hash_table(ht_hash_table* ht) {
     free(ht -> items);
     free(ht);
 }
+
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0; 
+    const int len_s = strlen(s);
+
+    for (int i = 0; i < len_s; i ++) {
+        // hash of the current character
+        hash += (long)pow(a, len_s - (i + 1)) * s[i];
+        // take modulus m to keep the hash value within the range [0, m - 1]
+        hash = hash % m;
+    }
+    return (int)hash;
+}
